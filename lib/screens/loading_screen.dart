820690d4f6f514a3bd2ab09d9f1171b13f5aaa42
tiagoorlamunder <tiago.orlamunder@gmail.com>
2022-10-28 13:50:11 -0300

    Passos seguidos

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+
+
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,17 +12,65 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getData() async {
+    var url = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) { // se a requisição foi feita com sucesso
+      var data = response.body;
+      print(data);  // imprima o resultado
+    } else {
+      print(response.statusCode);  // senão, imprima o código de erro
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  Future<void> checkLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // serviço de localização desabilitado. Não será possível continuar
+      return Future.error('O serviço de localização está desabilitado.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Sem permissão para acessar a localização
+        return Future.error('Sem permissão para acesso à localização');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // permissões negadas para sempre
+      return Future.error('A permissão para acesso a localização foi negada para sempre. Não é possível pedir permissão.');
+    }
+  }
+
+  Future<void> getLocation() async {
+    await checkLocationPermission();
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            // obtém a localização atual
+            getLocation();
           },
           child: const Text('Obter Localização'),
         ),
       ),
     );
   }
+
 }
